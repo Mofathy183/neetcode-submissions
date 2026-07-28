24b354bfab07260db7971438f41032b8de8b62d0
Mohamed Fathy <113822426+Mofathy183@users.noreply.github.com>
2026-07-28 13:24:11 +0300

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+
+        const hashMap = new Map<string, number>();
+
+        for (let i = 0; i < s.length; i++) {
+            let letterS = s[i] as string;
+            let letterT = t[i] as string;
+
+            let curS = hashMap.get(letterS) ?? 0;
+            hashMap.set(letterS, curS + 1);
+
+            let curT = hashMap.get(letterT) ?? 0;
+            hashMap.set(letterT, curT - 1);
+        }
+
+        return Array.from(hashMap.values()).every((count) => count === 0);
+    }
+}
